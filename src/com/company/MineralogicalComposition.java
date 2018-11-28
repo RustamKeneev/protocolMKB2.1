793792da2285bb2664e5f39f7013ce21db4ae83b
RustamKeneev <dr.rustamkeneev@gmail.com>
2018-11-29 01:43:42 +0600

    add Mineralogical Composition

--- src/com/company/MineralogicalComposition.java
+++ src/com/company/MineralogicalComposition.java
@@ -8,9 +8,16 @@ public class MineralogicalComposition extends JPanel {
         super(null);
     }
     public void paintComponent (Graphics g) {
-      g.draw3DRect(10,25,1074,100, true);
-      g.drawLine(375,25,375,120);
-      g.drawLine(731,25,731,120);
+      g.draw3DRect(10,25,1074,500, true);
+      g.drawLine(375,25,375,550);
+      g.drawLine(731,25,731,550);
       g.drawLine(10,55,1084,55);
+      g.drawLine(10,80,1084,80);
+      g.drawLine(10,105,1084,105);
+      g.drawLine(10,130,1084,130);
+      g.drawLine(10,155,1084,155);
+      g.drawLine(10,180,1084,180);
+      g.drawLine(10,205,1084,205);
+      g.drawLine(10,230,1084,230);
     }
 }
