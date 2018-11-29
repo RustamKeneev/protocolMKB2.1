8b7a6f5ad35395ff9d9fd9a1c2b150859c538e43
RustamKeneev <dr.rustamkeneev@gmail.com>
2018-11-29 14:57:01 +0600

    add Mineralogical Composition table

--- src/com/company/MineralogicalComposition.java
+++ src/com/company/MineralogicalComposition.java
@@ -8,9 +8,9 @@ public class MineralogicalComposition extends JPanel {
         super(null);
     }
     public void paintComponent (Graphics g) {
-      g.draw3DRect(10,25,1074,500, true);
-      g.drawLine(375,25,375,550);
-      g.drawLine(731,25,731,550);
+      g.draw3DRect(10,25,1074,780, true);
+      g.drawLine(375,25,375,805);
+      g.drawLine(751,25,751,805);
       g.drawLine(10,55,1084,55);
       g.drawLine(10,80,1084,80);
       g.drawLine(10,105,1084,105);
@@ -19,5 +19,26 @@ public class MineralogicalComposition extends JPanel {
       g.drawLine(10,180,1084,180);
       g.drawLine(10,205,1084,205);
       g.drawLine(10,230,1084,230);
+      g.drawLine(10,255,1084,255);
+      g.drawLine(10,280,1084,280);
+      g.drawLine(10,305,1084,305);
+      g.drawLine(10,330,1084,330);
+      g.drawLine(10,355,1084,355);
+      g.drawLine(10,380,1084,380);
+      g.drawLine(10,405,1084,405);
+      g.drawLine(10,430,1084,430);
+      g.drawLine(10,455,1084,455);
+      g.drawLine(375,480,1084,480);
+      g.drawLine(10,505,1084,505);
+      g.drawLine(10,530,375,530);
+      g.drawLine(10,555,375,555);
+      g.drawLine(10,580,375,580);
+      g.drawLine(10,605,375,605);
+      g.drawLine(10,630,375,630);
+      g.drawLine(10,655,375,655);
+      g.drawLine(10,680,375,680);
+      g.drawLine(10,705,1084,705);
+      g.drawLine(10,730,1084,730);
+      g.drawLine(10,780,1084,780);
     }
 }
